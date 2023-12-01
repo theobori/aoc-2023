c88495e94a2efe65cad038ba223ccac516f5be0c
Théo Bori <theo1.bori@epitech.eu>
2023-12-01 06:39:41 +0000

    feat: day1

---
+++ day1/part1/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+with open("../input.txt") as f:
+    lines = list(
+        filter(
+            lambda x: len(x) > 0,
+            f.read().split("\n")
+        )
+    )
+
+_sum = 0
+
+for line in lines:
+    line = list(filter(lambda x: x.isdigit(), line))
+    
+    if len(line) == 0:
+        continue
+    
+    _sum += int("".join((line[0], line[-1])))
+
+print(_sum)
